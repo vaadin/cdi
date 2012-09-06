231aaa9efd3205b884962d5aef0d193c1d5e64ab
Adam Bien <abien@adam-bien.com>
2012-09-06 21:11:52 +0200

    VaadinUIScoped factored out, because it was ugly

---
+++ vaadin-cdi/src/main/java/com/vaadin/cdi/VaadinUIScoped.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vaadin.cdi;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.inject.Scope;
+
+/**
+ * Annotation used for declaring bean class scope for VaadinUI beans
+ *
+ * @author Tomi Virkki / Vaadin Ltd
+ */
+@Scope
+// TODO: NormalScope
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+@Inherited
+public @interface VaadinUIScoped {
+}

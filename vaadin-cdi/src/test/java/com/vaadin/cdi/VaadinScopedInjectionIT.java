4f496cc2abcc019d8b7817414078cccd3cba915a
Adam Bien <abien@adam-bien.com>
2012-09-12 21:18:11 +0200

    HTTP client test implemented

--- vaadin-cdi/src/test/java/com/vaadin/cdi/VaadinScopedInjectionIT.java
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/VaadinScopedInjectionIT.java
@@ -1,44 +1,38 @@
 package com.vaadin.cdi;
 
-import org.jboss.arquillian.junit.Arquillian;
-import org.junit.runner.RunWith;
-import static com.vaadin.cdi.ArchiveProvider.*;
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.event.Event;
-import javax.inject.Inject;
+import static com.vaadin.cdi.ArchiveProvider.createJavaArchive;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.vaadin.cdi.views.MainUI;
+
 /**
- *
+ * 
  * @author adam-bien.com
  */
 @RunWith(Arquillian.class)
 public class VaadinScopedInjectionIT {
 
-    @Inject
-    VaadinScopedBean scoped;
-
-    @Inject
-    VaadinScopedBean anotherScoped;
-    
-    
-    
     @Deployment
-    public static JavaArchive deploy(){
-        return createJavaArchive(VaadinScopedBean.class);
-    }
-    
-
-    @Test
-    public void scopedIsScoped(){
-        assertTrue(scoped == anotherScoped);
+    public static JavaArchive deploy() {
+        return createJavaArchive(VaadinScopedBean.class, MainUI.class,
+                VaadinScopedBean.class);
     }
 
     @Test
-    public void allExistOnlyOnce(){
-        assertThat(VaadinScopedBean.COUNTER.get(),is(1));
+    public void scopedIsScoped() {
+        Client client = Client.create();
+        ClientResponse message = client.resource(
+                "http://localhost:8181/test/mainUI").get(ClientResponse.class);
+        assertThat(VaadinScopedBean.COUNTER.get(), is(1));
+        assertThat(MainUI.COUNTER.get(), is(1));
     }
 }

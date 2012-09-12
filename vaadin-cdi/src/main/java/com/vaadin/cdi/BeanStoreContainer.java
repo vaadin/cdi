ba23ecba72782e8470290df5c26d6a75b317ee0d
Adam Bien <abien@adam-bien.com>
2012-09-12 20:58:07 +0200

    UI null check added

--- vaadin-cdi/src/main/java/com/vaadin/cdi/BeanStoreContainer.java
+++ vaadin-cdi/src/main/java/com/vaadin/cdi/BeanStoreContainer.java
@@ -4,23 +4,32 @@
  */
 package com.vaadin.cdi;
 
-import com.vaadin.ui.UI;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
 
-@SuppressWarnings("serial")
+import com.vaadin.ui.UI;
+
 @SessionScoped
+@SuppressWarnings("serial")
 public class BeanStoreContainer implements Serializable {
 
     private final Map<Integer, UIBeanStore> beanStores = new HashMap<Integer, UIBeanStore>();
 
+    @Inject
+    Instance<UIBeanStore> beanStore;
+
     public UIBeanStore getBeanStore(final UI current) {
+        if (current == null) {
+            throw new IllegalArgumentException("UI cannot be null");
+        }
         final Integer key = current != null ? current.hashCode() : null;
         if (!beanStores.containsKey(key)) {
-            beanStores.put(key, new UIBeanStore());
+            beanStores.put(key, this.beanStore.get());
         }
         return beanStores.get(key);
     }

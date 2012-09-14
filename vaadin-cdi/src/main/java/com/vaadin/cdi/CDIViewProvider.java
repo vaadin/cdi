5a41444fcb3f1f4f4aeda1b94e74da4d1e92f7ff
Adam Bien <abien@adam-bien.com>
2012-09-14 17:52:29 +0200

    A view is associated with the current thread

--- vaadin-cdi/src/main/java/com/vaadin/cdi/CDIViewProvider.java
+++ vaadin-cdi/src/main/java/com/vaadin/cdi/CDIViewProvider.java
@@ -13,6 +13,7 @@ import javax.enterprise.util.AnnotationLiteral;
 import javax.inject.Inject;
 
 import com.vaadin.cdi.component.JaasTools;
+import com.vaadin.cdi.viewscope.CurrentView;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewProvider;
 import com.vaadin.ui.UI;
@@ -123,6 +124,7 @@ public class CDIViewProvider implements ViewProvider {
             View view = (View) beanManager.getReference(viewBean,
                     viewBean.getBeanClass(),
                     beanManager.createCreationalContext(viewBean));
+            CurrentView.set(view);
 
             return view;
         }

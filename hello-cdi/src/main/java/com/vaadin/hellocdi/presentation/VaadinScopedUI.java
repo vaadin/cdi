805ce74146f81ee33cd1b97169f6a3933d39d154
Adam Bien <abien@adam-bien.com>
2012-09-15 06:34:12 +0200

    Sample for VaadinScopedView implemented

--- hello-cdi/src/main/java/com/vaadin/hellocdi/presentation/VaadinScopedUI.java
+++ hello-cdi/src/main/java/com/vaadin/hellocdi/presentation/VaadinScopedUI.java
@@ -1,33 +1,26 @@
 package com.vaadin.hellocdi.presentation;
 
-import javax.inject.Inject;
-
 import com.vaadin.cdi.VaadinUI;
 import com.vaadin.cdi.VaadinUIScoped;
-import com.vaadin.cdi.VaadinView;
 import com.vaadin.server.WrappedRequest;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+/**
+ *
+ * @author adam-bien.com
+ */
 @VaadinUI
 @VaadinUIScoped
-public class CompositeUIWithViews extends UI{
-    
-    @Inject @VaadinView
-    DependentView dependentView;
-    
-    @Inject @VaadinView
-    VaadinScopedView scopedView;
+public class VaadinScopedUI extends UI {
 
     @Override
     protected void init(WrappedRequest request) {
         setSizeFull();
         VerticalLayout layout = new VerticalLayout();
         layout.setSizeFull();
-        layout.addComponent(new Label("+CompositeUI works " + dependentView + " " + scopedView));
+        layout.addComponent(new Label("+VaadinScopedView works"));
         setContent(layout);
-        
     }
-
 }

59bdfa10aff43f3e446a71dd34cf26d2bb88eb91
Peter Lehto <peter@vaadin.com>
2012-09-17 11:17:42 +0300

    Small code cleanup

--- vaadin-cdi/src/main/java/com/vaadin/cdi/UIScopedContext.java
+++ vaadin-cdi/src/main/java/com/vaadin/cdi/UIScopedContext.java
@@ -26,25 +26,6 @@ public class UIScopedContext implements Context {
         this.beanManager = beanManager;
     }
 
-    private UIBeanStore getCurrentBeanStore() {
-        return getCurrentBeanStore(UI.getCurrent());
-    }
-
-    private UIBeanStore getCurrentBeanStore(UI scopedView) {
-        Set<Bean<?>> beans = beanManager.getBeans(BeanStoreContainer.class);
-        if (beans.isEmpty()) {
-            String msg = "Unable to obtain bean store";
-            getLogger().severe(msg);
-            throw new IllegalStateException(msg);
-        }
-        final Bean<?> bean = beans.iterator().next();
-        final BeanStoreContainer container = (BeanStoreContainer) beanManager
-                .getReference(bean, bean.getBeanClass(),
-                        beanManager.createCreationalContext(bean));
-        return container.getBeanStore(scopedView);
-
-    }
-
     @Override
     public <T> T get(final Contextual<T> contextual) {
         return get(contextual);
@@ -65,10 +46,6 @@ public class UIScopedContext implements Context {
         return currentBeanStore.getBeanInstance(bean, creationalContext);
     }
 
-    public UI createScopedUI(Bean<UI> t, CreationalContext<UI> context) {
-        return t.create(context);
-    }
-
     @Override
     public Class<? extends Annotation> getScope() {
         return VaadinUIScoped.class;
@@ -77,6 +54,29 @@ public class UIScopedContext implements Context {
     @Override
     public boolean isActive() {
         return true;
+    }
+
+    private UI createScopedUI(Bean<UI> t, CreationalContext<UI> context) {
+        return t.create(context);
+    }
+
+    private UIBeanStore getCurrentBeanStore() {
+        return getCurrentBeanStore(UI.getCurrent());
+    }
+
+    private UIBeanStore getCurrentBeanStore(UI scopedView) {
+        Set<Bean<?>> beans = beanManager.getBeans(BeanStoreContainer.class);
+        if (beans.isEmpty()) {
+            String msg = "Unable to obtain bean store";
+            getLogger().severe(msg);
+            throw new IllegalStateException(msg);
+        }
+        final Bean<?> bean = beans.iterator().next();
+        final BeanStoreContainer container = (BeanStoreContainer) beanManager
+                .getReference(bean, bean.getBeanClass(),
+                        beanManager.createCreationalContext(bean));
+        return container.getBeanStore(scopedView);
+
     }
 
     private static Logger getLogger() {

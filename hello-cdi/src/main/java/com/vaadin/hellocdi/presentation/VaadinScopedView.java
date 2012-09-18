93fac7882de2b43bc066bccced596b0e52d63414
Peter Lehto <peter@vaadin.com>
2012-09-18 10:41:11 +0300

    Removed @VaadinViewScoped annotation from test applications

--- hello-cdi/src/main/java/com/vaadin/hellocdi/presentation/VaadinScopedView.java
+++ hello-cdi/src/main/java/com/vaadin/hellocdi/presentation/VaadinScopedView.java
@@ -1,13 +1,11 @@
 package com.vaadin.hellocdi.presentation;
 
 import com.vaadin.cdi.VaadinView;
-import com.vaadin.cdi.viewscope.VaadinViewScoped;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 
 @VaadinView
-@VaadinViewScoped
-public class VaadinScopedView implements View{
+public class VaadinScopedView implements View {
 
     @Override
     public void enter(ViewChangeEvent event) {
--- hello-cdi/src/main/java/com/vaadin/hellocdi/presentation/namedevents/OkButtonListener.java
+++ hello-cdi/src/main/java/com/vaadin/hellocdi/presentation/namedevents/OkButtonListener.java
@@ -3,12 +3,18 @@ package com.vaadin.hellocdi.presentation.namedevents;
 import javax.enterprise.event.Observes;
 import javax.inject.Named;
 
-import com.vaadin.cdi.viewscope.VaadinViewScoped;
+import com.vaadin.cdi.VaadinUIScoped;
 
-@VaadinViewScoped
+@VaadinUIScoped
 public class OkButtonListener {
 
-    public void onOkButtonClicked(@Observes @Named("ok") String message){
+    public OkButtonListener() {
+        System.out.println("instantiating new " + this);
+    }
+
+    public void onOkButtonClicked(@Observes
+    @Named("ok")
+    String message) {
         System.out.println("Received: " + message);
     }
 }

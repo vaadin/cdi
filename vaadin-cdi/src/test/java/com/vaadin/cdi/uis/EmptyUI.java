18cb255323b9b1b619921b1be99c4fbb0a1d95aa
Adam Bien <abien@adam-bien.com>
2012-09-18 18:54:46 +0200

    Initial implementation of integration test for UIBeanStore

---
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/uis/EmptyUI.java
@@ -0,0 +1,35 @@
+package com.vaadin.cdi.uis;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import com.vaadin.cdi.UIBeanStore;
+import com.vaadin.cdi.VaadinUI;
+import com.vaadin.cdi.VaadinUIScoped;
+import com.vaadin.server.WrappedRequest;
+import com.vaadin.ui.UI;
+
+@VaadinUI
+@VaadinUIScoped
+public class EmptyUI extends UI{
+
+    private final static AtomicInteger COUNTER = new AtomicInteger(0);
+    
+    @PostConstruct
+    public void initialize(){
+        COUNTER.incrementAndGet();
+        
+    }
+    @Override
+    protected void init(WrappedRequest request) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    public int getNumberOfInstances(){
+        return COUNTER.get();
+    }
+
+}

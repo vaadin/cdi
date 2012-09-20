5d3a2b682bfc24846d872de3a5d4a18cc86fb59b
Adam Bien <abien@adam-bien.com>
2012-09-20 05:13:18 +0200

    Additional test case implemented

---
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/views/OneAndOnlyViewWithoutPathAndAnnotation.java
@@ -0,0 +1,16 @@
+package com.vaadin.cdi.views;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+
+/**
+ *
+ * @author adam-bien.com
+ */
+public class OneAndOnlyViewWithoutPathAndAnnotation implements View{
+
+    public void enter(ViewChangeEvent event) {
+    }
+
+    
+}

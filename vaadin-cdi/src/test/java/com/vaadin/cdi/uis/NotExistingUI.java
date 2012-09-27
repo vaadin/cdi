2054961d9ff714bb559dbdfb844b7daa760918b0
Adam Bien <abien@adam-bien.com>
2012-09-27 15:15:22 +0200

    DanglingView tests completed

---
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/uis/NotExistingUI.java
@@ -0,0 +1,15 @@
+package com.vaadin.cdi.uis;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+/**
+ * @author: adam-bien.com
+ */
+public class NotExistingUI extends UI
+{
+    @Override
+    protected void init(VaadinRequest request) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}

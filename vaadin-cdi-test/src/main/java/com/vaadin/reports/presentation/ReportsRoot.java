15c69a7b8c7a52fc35d72b22854c18995155bead
Peter Lehto <peter@vaadin.com>
2012-10-02 15:58:12 +0300

    Fixed non-compilable test uis

--- vaadin-cdi-test/src/main/java/com/vaadin/reports/presentation/ReportsRoot.java
+++ vaadin-cdi-test/src/main/java/com/vaadin/reports/presentation/ReportsRoot.java
@@ -4,21 +4,19 @@ import java.util.Date;
 
 import javax.inject.Inject;
 
-import com.vaadin.cdi.Mapping;
 import com.vaadin.reports.business.issues.boundary.BugTracking;
-import com.vaadin.server.WrappedRequest;
+import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.UI;
 
-@Mapping(value = "reports")
 public class ReportsRoot extends UI {
 
     @Inject
     BugTracking bt;
 
     @Override
-    protected void init(WrappedRequest request) {
+    protected void init(VaadinRequest request) {
         addComponent(new Button("New Bug", new Button.ClickListener() {
 
             @Override

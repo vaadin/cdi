a8a72de148ec041db446b55f90b42524d9940afb
Adam Bien <abien@adam-bien.com>
2012-10-02 17:04:38 -0700

    Merge completed

--- vaadin-cdi-test/src/main/java/com/vaadin/customer/presentation/CustomerRoot.java
+++ vaadin-cdi-test/src/main/java/com/vaadin/customer/presentation/CustomerRoot.java
@@ -6,10 +6,7 @@ import com.vaadin.cdi.CDIViewProvider;
 import com.vaadin.cdi.VaadinUI;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.Navigator.SimpleViewDisplay;
-<<<<<<< HEAD
-=======
 import com.vaadin.navigator.ViewDisplay;
->>>>>>> 15c69a7b8c7a52fc35d72b22854c18995155bead
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.UI;
 

2adbdbb33e10a063689476c8debc351675f16762
Adam Bien <abien@adam-bien.com>
2012-10-08 08:38:28 +0200

    JavaDoc extensions

--- vaadin-cdi/src/main/java/com/vaadin/cdi/Root.java
+++ vaadin-cdi/src/main/java/com/vaadin/cdi/Root.java
@@ -1,14 +1,24 @@
 package com.vaadin.cdi;
 
-import javax.inject.Qualifier;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.inject.Qualifier;
+
 
 /**
  * UIs annotated with @Root are bound to the context path of the application.
+ * There can be only one UI annotated with @Root per application.
+ * 
+ * <pre>
+ *     <code>
+ *  @Root
+ *  @VaadinUI
+ *  public class EntryPoint extends UI {}
+ *     </code>
+ * </pre>
  * 
  * @author adam-bien.com
  */

b134c635e0c3399f176ca2f3bfa84bafb21e60f5
Adam Bien <abien@adam-bien.com>
2013-02-13 19:01:02 +0100

    Version increased, superfluous import removed

--- vaadin-cdi/src/test/java/com/vaadin/cdi/uis/PlainAlternativeUI.java
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/uis/PlainAlternativeUI.java
@@ -1,19 +1,18 @@
 /*
  * Copyright 2012 Vaadin Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package com.vaadin.cdi.uis;
 
 import com.vaadin.cdi.VaadinUI;
@@ -21,7 +20,6 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
-import sun.security.x509.IssuerAlternativeNameExtension;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Alternative;
@@ -30,8 +28,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 @VaadinUI
 @Alternative
 public class PlainAlternativeUI extends UI {
-
-
 
     private final static AtomicInteger COUNTER = new AtomicInteger(0);
     private int clickCount;
@@ -63,5 +59,4 @@ public class PlainAlternativeUI extends UI {
     public static void resetCounter() {
         COUNTER.set(0);
     }
-
 }

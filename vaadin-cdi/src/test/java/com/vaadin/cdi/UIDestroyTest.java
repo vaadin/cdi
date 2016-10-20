b2291b31bcacc868c694c499fab081c202dfc9b5
kumm <kumm@webstar.hu>
2016-10-20 22:54:57 +0200

    test for UI destroy on viewchange

---
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/UIDestroyTest.java
@@ -0,0 +1,54 @@
+package com.vaadin.cdi;
+
+import com.vaadin.cdi.internal.Conventions;
+import com.vaadin.cdi.uis.DestroyUI;
+import com.vaadin.cdi.views.TestView;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class UIDestroyTest extends AbstractManagedCDIIntegrationTest {
+
+    private String uri;
+    private String uiId;
+
+    @Deployment(testable = false)
+    public static WebArchive deployment() {
+        return ArchiveProvider.createWebArchive("uiDestroy",
+                DestroyUI.class,
+                TestView.class);
+    }
+
+    @Test
+    public void testViewChangeTriggersClosedUIDestroy() throws Exception {
+        resetCounts();
+        uri = Conventions.deriveMappingForUI(DestroyUI.class);
+        openWindow(uri);
+        uiId = findElement(DestroyUI.UIID_ID).getText();
+        assertDestroyCount(0);
+        //close first UI
+        clickAndWait(DestroyUI.CLOSE_BTN_ID);
+
+        //open new UI
+        openWindow(uri);
+        assertDestroyCount(0);
+
+        Thread.sleep(5000); //AbstractVaadinContext.CLEANUP_DELAY
+
+        //ViewChange event triggers a cleanup
+        clickAndWait(DestroyUI.NAVIGATE_BTN_ID);
+
+        //first UI cleaned up
+        assertDestroyCount(1);
+    }
+
+    private void assertDestroyCount(int count) throws IOException {
+        assertThat(getCount(DestroyUI.DESTROY_COUNT + uiId), is(count));
+    }
+
+}

ba1c0095bdab7bfb23906aeee9138f5ed08f2457
kumm <kumm@webstar.hu>
2016-11-17 23:49:02 +0200

    Do not expect undeclared DeploymentException in deployment tests. Glassfish managed does not throw it.

--- vaadin-cdi/src/test/java/com/vaadin/cdi/CDIIntegrationWithConflictingUIPathTest.java
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/CDIIntegrationWithConflictingUIPathTest.java
@@ -2,13 +2,10 @@ package com.vaadin.cdi;
 
 import com.vaadin.cdi.uis.AnotherPathCollisionUI;
 import com.vaadin.cdi.uis.PathCollisionUI;
-import org.jboss.arquillian.container.spi.client.container.DeploymentException;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.InSequence;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
-
-import java.net.MalformedURLException;
 
 import static org.junit.Assert.fail;
 
@@ -26,21 +23,11 @@ public class CDIIntegrationWithConflictingUIPathTest extends
      * Tests invalid deployment of multiple roots within a WAR Should be started
      * first -- Arquillian deployments are not perfectly isolated.
      */
-    @Test
+    @Test(expected = Exception.class)
     @InSequence(-2)
-    public void uiPathCollisionBreaksDeployment() throws MalformedURLException {
-        try {
-            System.out.println("DEPLOYING");
-            // Deployment doesn't declare that it can throw DeploymentException
-            // De-facto it can
-            deployer.deploy("uiPathCollision");
-            System.out.println("Deployed");
-            fail("Duplicate deployment paths should not be deployable");
-            throw new DeploymentException(null);
-        } catch (DeploymentException e) {
-            // Correct response
-            System.out.println("Exiting try block");
-        }
+    public void uiPathCollisionBreaksDeployment() throws Exception {
+        deployer.deploy("uiPathCollision");
+        fail("Duplicate deployment paths should not be deployable");
     }
 
 }

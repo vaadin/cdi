2cf5157f41aa93b0366b2a2b68acc164f860c3d3
kumm <kumm@webstar.hu>
2017-01-28 17:44:26 +0100

    Test for session context destroy on http session invalidation

--- vaadin-cdi/src/test/java/com/vaadin/cdi/SessionContextTest.java
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/SessionContextTest.java
@@ -6,6 +6,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SessionContextTest extends AbstractManagedCDIIntegrationTest {
@@ -31,9 +32,34 @@ public class SessionContextTest extends AbstractManagedCDIIntegrationTest {
     }
 
     @Test
-    public void testSessionCloseDestroysSessionContext() throws Exception {
+    public void testVaadinSessionCloseDestroysSessionContext() throws Exception {
         Assert.assertEquals(0, getCount(SessionUI.SessionScopedBean.DESTROY_COUNT));
         clickAndWait(SessionUI.INVALIDATEBTN_ID);
         Assert.assertEquals(1, getCount(SessionUI.SessionScopedBean.DESTROY_COUNT));
+    }
+
+    @Test
+    public void testHttpSessionCloseDestroysSessionContext() throws Exception {
+        Assert.assertEquals(0, getCount(SessionUI.SessionScopedBean.DESTROY_COUNT));
+        clickAndWait(SessionUI.HTTP_INVALIDATEBTN_ID);
+        Assert.assertEquals(1, getCount(SessionUI.SessionScopedBean.DESTROY_COUNT));
+    }
+
+    @Test
+    @Ignore
+    //ignored because it's slow, and expiration should be same as session close
+    public void testHttpSessionExpirationDestroysSessionContext() throws Exception {
+        Assert.assertEquals(0, getCount(SessionUI.SessionScopedBean.DESTROY_COUNT));
+        clickAndWait(SessionUI.EXPIREBTN_ID);
+        boolean destroyed = false;
+        for (int i=0; i<60; i++) {
+            Thread.sleep(1000);
+            if (getCount(SessionUI.SessionScopedBean.DESTROY_COUNT) > 0) {
+                System.out.printf("session expired after %d seconds\n", i);
+                destroyed = true;
+                break;
+            }
+        }
+        Assert.assertTrue(destroyed);
     }
 }

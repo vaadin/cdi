efd6aaf3e7ecca80a80d0ab6d8388cf850d46aec
kumm <kumm@webstar.hu>
2017-08-20 15:53:30 +0200

    Note about VaadinContextualStorage, implement getBean

--- vaadin-cdi/src/main/java/com/vaadin/cdi/internal/VaadinContextualStorage.java
+++ vaadin-cdi/src/main/java/com/vaadin/cdi/internal/VaadinContextualStorage.java
@@ -1,22 +1,33 @@
 package com.vaadin.cdi.internal;
 
+import org.apache.deltaspike.core.util.context.ContextualStorage;
+
 import javax.enterprise.context.spi.Contextual;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.PassivationCapable;
-
-import org.apache.deltaspike.core.util.context.ContextualStorage;
 
 /**
  * Customized version of ContextualStorage to also handle beans that are not
  * PassivationCapable. Such beans are used as their own keys, which is not ideal
  * but should work in most single-JVM environments.
- * 
+ *
+ * Note:
+ * Supporting non-PassivationCapable beans is theoretical.
+ * Not required by the spec, but in reality beans are PassivationCapable.
+ * Even for non serializable bean classes.
+ *
+ * CDI implementations use PassivationCapable beans,
+ * because injecting non serializable proxies might block serialization of
+ * bean instances in a passivation capable context.
+ *
  * @see ContextualStorage
  */
 public class VaadinContextualStorage extends ContextualStorage {
+    private final BeanManager beanManager;
 
     public VaadinContextualStorage(BeanManager beanManager, boolean concurrent) {
         super(beanManager, concurrent, true);
+        this.beanManager = beanManager;
     }
     
     @Override
@@ -28,4 +39,12 @@ public class VaadinContextualStorage extends ContextualStorage {
         }
     }
 
+    @Override
+    public Contextual<?> getBean(Object beanKey) {
+        if (beanKey instanceof String) {
+            return beanManager.getPassivationCapableBean((String) beanKey);
+        } else {
+            return (Contextual<?>) beanKey;
+        }
+    }
 }

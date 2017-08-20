42714e35d77666dd5897b1965cb20e1e9c8e199d
kumm <kumm@webstar.hu>
2017-08-20 15:26:38 +0200

    Define new cleanup behaviour of UI scoped context
    Cleanup have to happen immediately on session close, and on UI close.

--- vaadin-cdi/src/test/java/com/vaadin/cdi/UIDestroyTest.java
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/UIDestroyTest.java
@@ -5,6 +5,7 @@ import com.vaadin.cdi.uis.DestroyUI;
 import com.vaadin.cdi.views.TestView;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -14,7 +15,6 @@ import static org.junit.Assert.assertThat;
 
 public class UIDestroyTest extends AbstractManagedCDIIntegrationTest {
 
-    private String uri;
     private String uiId;
 
     @Deployment(testable = false)
@@ -24,31 +24,30 @@ public class UIDestroyTest extends AbstractManagedCDIIntegrationTest {
                 TestView.class);
     }
 
-    @Test
-    public void testViewChangeTriggersClosedUIDestroy() throws Exception {
+    @Before
+    public void setUp() throws IOException {
         resetCounts();
-        uri = Conventions.deriveMappingForUI(DestroyUI.class);
+        String uri = Conventions.deriveMappingForUI(DestroyUI.class);
         openWindow(uri);
         uiId = findElement(DestroyUI.UIID_ID).getText();
         assertDestroyCount(0);
-        //close first UI
+    }
+
+    @Test
+    public void testUiCloseTriggersDestroy() throws Exception {
         clickAndWait(DestroyUI.CLOSE_BTN_ID);
+        assertDestroyCount(1);
+    }
 
-        //open new UI
-        openWindow(uri);
-        assertDestroyCount(0);
-
-        Thread.sleep(5000); //AbstractVaadinContext.CLEANUP_DELAY
-
-        //ViewChange event triggers a cleanup
-        clickAndWait(DestroyUI.NAVIGATE_BTN_ID);
-
-        //first UI cleaned up
+    @Test
+    public void testSessionCloseDestroysUIContext() throws Exception {
+        clickAndWait(DestroyUI.CLOSE_SESSION_BTN_ID);
         assertDestroyCount(1);
     }
 
     private void assertDestroyCount(int count) throws IOException {
         assertThat(getCount(DestroyUI.DESTROY_COUNT + uiId), is(count));
+        assertThat(getCount(DestroyUI.UIScopedBean.DESTROY_COUNT + uiId), is(count));
     }
 
 }

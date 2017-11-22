d438bf648345b9f0ca44298476c746928a36bf65
Teemu Suo-Anttila <teemusa@vaadin.com>
2017-11-22 10:52:54 +0200

    Fix CDIUIProviderViewDecetionTest to not use Arquillian

--- vaadin-cdi/src/test/java/com/vaadin/cdi/CDIUIProviderViewDetectionTest.java
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/CDIUIProviderViewDetectionTest.java
@@ -1,27 +1,57 @@
 package com.vaadin.cdi;
 
-import javax.inject.Inject;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import com.vaadin.cdi.uis.PushStateSubUI;
 import com.vaadin.cdi.uis.PushStateUI;
 import com.vaadin.cdi.uis.RootUI;
 import com.vaadin.cdi.uis.SubUI;
+import com.vaadin.ui.UI;
 
-public class CDIUIProviderViewDetectionTest
-        extends AbstractManagedCDIIntegrationTest {
+@RunWith(MockitoJUnitRunner.class)
+public class CDIUIProviderViewDetectionTest {
 
-    @Deployment
-    public static WebArchive createJavaTestArchive() {
-        return ArchiveProvider.createWebArchive("uiProvider", RootUI.class,
-                SubUI.class, PushStateUI.class);
+    @Mock
+    BeanManager beanManager;
+
+    @InjectMocks
+    CDIUIProvider cut;
+
+    @Before
+    public void setUp() throws Exception {
+        final Set<Bean<?>> beans = new LinkedHashSet<>(Arrays.asList(
+                mockBean(RootUI.class), mockBean(SubUI.class),
+                mockBean(PushStateUI.class), mockBean(PushStateSubUI.class)));
+
+        when(beanManager.getBeans(eq(UI.class), isA(Annotation.class)))
+                .thenReturn(beans);
     }
 
-    @Inject
-    CDIUIProvider cut;
+    private Bean<?> mockBean(Class<?> beanClass) {
+        final Bean bean = mock(Bean.class);
+        when(bean.getBeanClass()).thenReturn(beanClass);
+        return bean;
+    }
 
     @Test
     public void testRootUI() {
@@ -51,8 +81,27 @@ public class CDIUIProviderViewDetectionTest
                 cut.getUIBeanWithMapping("pushState/view/param").getBeanClass()
                         .getCanonicalName());
         Assert.assertEquals(PushStateUI.class.getCanonicalName(),
-                cut.getUIBeanWithMapping("pushState/view/with/multiple/params").getBeanClass()
-                        .getCanonicalName());
+                cut.getUIBeanWithMapping("pushState/view/with/multiple/params")
+                        .getBeanClass().getCanonicalName());
     }
 
+    @Test
+    @Ignore("Longest UI Path Detection not implemented yet.")
+    public void testPushSateSubUI() {
+        Assert.assertEquals(PushStateSubUI.class.getCanonicalName(),
+                cut.getUIBeanWithMapping("pushState/sub").getBeanClass()
+                        .getCanonicalName());
+        Assert.assertEquals(PushStateSubUI.class.getCanonicalName(),
+                cut.getUIBeanWithMapping("pushState/sub/").getBeanClass()
+                        .getCanonicalName());
+        Assert.assertEquals(PushStateSubUI.class.getCanonicalName(),
+                cut.getUIBeanWithMapping("pushState/sub/view").getBeanClass()
+                        .getCanonicalName());
+        Assert.assertEquals(PushStateSubUI.class.getCanonicalName(),
+                cut.getUIBeanWithMapping("pushState/sub/view/param")
+                        .getBeanClass().getCanonicalName());
+        Assert.assertEquals(PushStateSubUI.class.getCanonicalName(), cut
+                .getUIBeanWithMapping("pushState/sub/view/with/multiple/params")
+                .getBeanClass().getCanonicalName());
+    }
 }

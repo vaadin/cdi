cb8ec56a6ce4c8b04a3ac9599f36ae9962f7d9c4
kumm <kumm@webstar.hu>
2018-06-26 21:04:05 +0200

    Remove logging of normal scoped components during deploy

--- vaadin-cdi/src/main/java/com/vaadin/cdi/VaadinExtension.java
+++ vaadin-cdi/src/main/java/com/vaadin/cdi/VaadinExtension.java
@@ -21,7 +21,6 @@ import com.vaadin.cdi.context.ContextWrapper;
 import com.vaadin.cdi.context.UIScopedContext;
 import com.vaadin.cdi.context.VaadinServiceScopedContext;
 import com.vaadin.cdi.context.VaadinSessionScopedContext;
-import com.vaadin.flow.component.Component;
 import org.apache.deltaspike.core.util.context.AbstractContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,13 +28,9 @@ import org.slf4j.LoggerFactory;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.AfterBeanDiscovery;
 import javax.enterprise.inject.spi.AfterDeploymentValidation;
-import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.Extension;
-import javax.enterprise.inject.spi.ProcessManagedBean;
 import java.lang.annotation.Annotation;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * CDI Extension needed to register Vaadin scopes to the runtime.
@@ -45,46 +40,14 @@ public class VaadinExtension implements Extension {
     private VaadinServiceScopedContext serviceScopedContext;
     private UIScopedContext uiScopedContext;
 
-    private List<String> normalScopedComponentWarnings = new LinkedList<>();
-
     public void initializeContexts(@Observes AfterDeploymentValidation adv, BeanManager beanManager) {
         serviceScopedContext.init(beanManager);
         uiScopedContext.init(beanManager);
     }
 
-    void processManagedBean(@Observes ProcessManagedBean pmb,
-            final BeanManager beanManager) {
-        Bean<?> bean = pmb.getBean();
-        Class<?> beanClass = bean.getBeanClass();
-        Class<? extends Annotation> beanScope = bean.getScope();
-
-        if (Component.class.isAssignableFrom(beanClass)
-                && beanManager.isNormalScope(beanScope)) {
-            normalScopedComponentWarnings.add("@"
-                    + String.format("%-20s", beanScope.getSimpleName()) + " "
-                    + beanClass.getCanonicalName());
-        }
-
-    }
-
     void afterBeanDiscovery(
             @Observes final AfterBeanDiscovery afterBeanDiscovery,
             final BeanManager beanManager) {
-
-        if (normalScopedComponentWarnings.size() > 0) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("The following Vaadin components are injected into "
-                    + "normal scoped contexts:\n");
-            for (String proxiedComponent : normalScopedComponentWarnings) {
-                sb.append("   ");
-                sb.append(proxiedComponent);
-                sb.append("\n");
-            }
-            sb.append("This approach uses proxy objects and is "
-                    + "not supported in Vaadin framework.");
-            getLogger().error(sb.toString());
-        }
-
         serviceScopedContext = new VaadinServiceScopedContext(beanManager);
         uiScopedContext = new UIScopedContext(beanManager);
         addContext(afterBeanDiscovery, serviceScopedContext, null);

23695b0865af9870538db6157b1bc158413819b3
Denis Anisimov <denis@vaadin.com>
2018-08-10 11:26:11 +0300

    Replace flow artifact with server, client and html-components

--- vaadin-cdi-itest/src/test/java/com/vaadin/cdi/itest/ArchiveProvider.java
+++ vaadin-cdi-itest/src/test/java/com/vaadin/cdi/itest/ArchiveProvider.java
@@ -16,6 +16,8 @@
 
 package com.vaadin.cdi.itest;
 
+import java.util.function.Consumer;
+
 import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
@@ -23,32 +25,33 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.jboss.shrinkwrap.resolver.api.maven.PomEquippedResolveStage;
 
-import java.util.function.Consumer;
-
 public class ArchiveProvider {
 
-    public static WebArchive createWebArchive(String warName, Consumer<WebArchive> customizer) {
+    public static WebArchive createWebArchive(String warName,
+            Consumer<WebArchive> customizer) {
         WebArchive archive = base(warName);
         customizer.accept(archive);
         return archive;
     }
 
-    public static WebArchive createWebArchive(String warName, Class... classes) {
-        return createWebArchive(warName, archive -> archive.addClasses(classes));
+    public static WebArchive createWebArchive(String warName,
+            Class... classes) {
+        return createWebArchive(warName,
+                archive -> archive.addClasses(classes));
     }
 
     private static WebArchive base(String warName) {
-        PomEquippedResolveStage pom = Maven.configureResolver()
-                .workOffline()
+        PomEquippedResolveStage pom = Maven.configureResolver().workOffline()
                 .loadPomFromFile("pom.xml");
-        return ShrinkWrap
-                .create(WebArchive.class, warName + ".war")
-                .addAsLibraries(
-                        pom.resolve("com.vaadin:vaadin-cdi")
-                                .withTransitivity().asFile())
-                .addAsLibraries(
-                        pom.resolve("com.vaadin:flow")
-                                .withTransitivity().asFile())
+        return ShrinkWrap.create(WebArchive.class, warName + ".war")
+                .addAsLibraries(pom.resolve("com.vaadin:vaadin-cdi")
+                        .withTransitivity().asFile())
+                .addAsLibraries(pom.resolve("com.vaadin:flow-server")
+                        .withTransitivity().asFile())
+                .addAsLibraries(pom.resolve("com.vaadin:flow-client")
+                        .withTransitivity().asFile())
+                .addAsLibraries(pom.resolve("com.vaadin:flow-html-components")
+                        .withTransitivity().asFile())
                 .addAsLibraries(
                         pom.resolve("org.webjars.bowergithub.polymer:polymer")
                                 .withTransitivity().asFile())

69aefad312bfad5a61a06b918bb6a6788b06bc7a
Denis <denis@vaadin.com>
2019-10-15 11:43:16 +0300

    Fix unit tests for 2.1 (#299)

--- vaadin-cdi/src/test/java/com/vaadin/cdi/TestCdiVaadinServletService.java
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/TestCdiVaadinServletService.java
@@ -16,26 +16,31 @@
 
 package com.vaadin.cdi;
 
+import javax.enterprise.inject.spi.BeanManager;
+import javax.servlet.ServletContext;
+
+import java.util.Properties;
+
 import com.vaadin.flow.function.DeploymentConfiguration;
 import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.flow.server.VaadinSession;
-
-import javax.enterprise.inject.spi.BeanManager;
-import javax.servlet.ServletContext;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class TestCdiVaadinServletService extends CdiVaadinServletService {
 
-    public TestCdiVaadinServletService(BeanManager beanManager, String servletName) {
-        super(
-                mock(CdiVaadinServlet.class),
-                mock(DeploymentConfiguration.class),
+    public TestCdiVaadinServletService(BeanManager beanManager,
+            String servletName) {
+        super(mock(CdiVaadinServlet.class), mock(DeploymentConfiguration.class),
                 beanManager);
         when(getServlet().getServletName()).thenReturn(servletName);
         when(getServlet().getService()).thenReturn(this);
-        when(getServlet().getServletContext()).thenReturn(mock(ServletContext.class));
+        when(getServlet().getServletContext())
+                .thenReturn(mock(ServletContext.class));
+        DeploymentConfiguration config = getDeploymentConfiguration();
+        Properties properties = new Properties();
+        when(config.getInitParameters()).thenReturn(properties);
     }
 
     @Override

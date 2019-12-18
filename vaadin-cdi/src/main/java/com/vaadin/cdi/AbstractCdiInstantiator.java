5d9de0913c7d1a5651efdbf7f934fe0ef814a0ff
Pekka Hyvönen <pekka@vaadin.com>
2019-12-18 11:33:22 +0200

    Add usage statistics

--- vaadin-cdi/src/main/java/com/vaadin/cdi/AbstractCdiInstantiator.java
+++ vaadin-cdi/src/main/java/com/vaadin/cdi/AbstractCdiInstantiator.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2000-2019 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.vaadin.cdi;
 
 import javax.enterprise.inject.spi.BeanManager;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Stream;
 
+import com.vaadin.flow.internal.UsageStatistics;
 import org.apache.deltaspike.core.api.provider.BeanProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +44,7 @@ abstract public class AbstractCdiInstantiator implements Instantiator {
 
     @Override
     public boolean init(VaadinService service) {
+        UsageStatistics.markAsUsed("flow/CdiInstantiator", null);
         delegate = new DefaultInstantiator(service);
         return delegate.init(service)
                 && getServiceClass().isAssignableFrom(service.getClass());

055653b8196a90e6adb06e27ae562ade7fbae286
Denis <denis@vaadin.com>
2020-11-19 11:21:43 +0300

    chore: Use Flow 6.0 version and update unit test (#358)
    Fixes #357

--- vaadin-cdi/src/test/java/com/vaadin/cdi/CdiVaadinServletTest.java
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/CdiVaadinServletTest.java
@@ -16,8 +16,14 @@
 
 package com.vaadin.cdi;
 
-import com.vaadin.cdi.context.ServiceUnderTestContext;
-import com.vaadin.flow.server.VaadinServletService;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import java.util.Collections;
+
 import org.apache.deltaspike.core.api.provider.BeanProvider;
 import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
 import org.junit.After;
@@ -26,12 +32,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
-import javax.enterprise.inject.spi.BeanManager;
-import javax.inject.Inject;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import java.util.Collections;
+import com.vaadin.cdi.context.ServiceUnderTestContext;
+import com.vaadin.flow.di.Lookup;
+import com.vaadin.flow.di.ResourceProvider;
+import com.vaadin.flow.server.VaadinServletService;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertThat;
@@ -47,13 +51,21 @@ public class CdiVaadinServletTest {
     @Before
     public void setUp() throws ServletException {
         final ServletConfig servletConfig = Mockito.mock(ServletConfig.class);
-        final ServletContext servletContext = Mockito.mock(ServletContext.class);
+        final ServletContext servletContext = Mockito
+                .mock(ServletContext.class);
+
+        Lookup lookup = Mockito.mock(Lookup.class);
+        ResourceProvider provider = Mockito.mock(ResourceProvider.class);
+        Mockito.when(lookup.lookup(ResourceProvider.class))
+                .thenReturn(provider);
+        Mockito.when(servletContext.getAttribute(Lookup.class.getName()))
+                .thenReturn(lookup);
+
         Mockito.when(servletConfig.getInitParameterNames())
                 .thenReturn(Collections.emptyEnumeration());
         Mockito.when(servletConfig.getServletContext())
                 .thenReturn(servletContext);
-        Mockito.when(servletConfig.getServletName())
-                .thenReturn("test");
+        Mockito.when(servletConfig.getServletName()).thenReturn("test");
         Mockito.when(servletContext.getInitParameterNames())
                 .thenReturn(Collections.emptyEnumeration());
         servlet = new CdiVaadinServlet();

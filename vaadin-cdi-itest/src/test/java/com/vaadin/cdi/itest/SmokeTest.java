9fe38b6dfe1a959bb46d2d8fcd389f2160382aa5
Marco Collovati <marco@vaadin.com>
2024-04-15 11:08:17 +0200

    improve test

--- vaadin-cdi-itest/src/test/java/com/vaadin/cdi/itest/SmokeTest.java
+++ vaadin-cdi-itest/src/test/java/com/vaadin/cdi/itest/SmokeTest.java
@@ -82,13 +82,14 @@ public class SmokeTest extends AbstractCdiTest {
             if (prevUuid != null) {
                 Assert.assertEquals("UUID should not have been changed",
                         prevUuid, uuid);
+            } else {
+                prevUuid = uuid;
             }
             String counter = findElement(By.id("CLICK_COUNTER")).getText();
             Assert.assertEquals(
                     "Parameter and counter should have the same value",
                     "P:" + i + ", C:" + i, counter);
 
-            prevUuid = uuid;
             prevCounter.set(counter);
 
             $("a").first().click();

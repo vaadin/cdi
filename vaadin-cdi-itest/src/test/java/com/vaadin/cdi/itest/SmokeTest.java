0c23cd1581b6c1dce5be405e95d770a1b8b334fc
Marco Collovati <marco@vaadin.com>
2024-04-16 13:54:03 +0200

    test: add test for proxied views (#458)
    Ensure changes in vaadin/flow#19151 works correctly on CDI

--- vaadin-cdi-itest/src/test/java/com/vaadin/cdi/itest/SmokeTest.java
+++ vaadin-cdi-itest/src/test/java/com/vaadin/cdi/itest/SmokeTest.java
@@ -16,13 +16,18 @@
 
 package com.vaadin.cdi.itest;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import com.vaadin.cdi.itest.smoke.CdiView;
+import com.vaadin.cdi.itest.smoke.ProxiedNavigationTargetView;
+import com.vaadin.cdi.itest.smoke.UselessInterceptor;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 @SuppressWarnings("ArquillianTooManyDeployment")
 public class SmokeTest extends AbstractCdiTest {
@@ -30,7 +35,7 @@ public class SmokeTest extends AbstractCdiTest {
     @Deployment(name = "noncdi", testable = false)
     public static WebArchive createCdiServletDisabledDeployment() {
         return ArchiveProvider.createWebArchive("noncdi-test",
-                CdiView.class)
+                        CdiView.class)
                 .addAsWebInfResource(ArchiveProvider.class.getClassLoader()
                         .getResource("disablecdi-web.xml"), "web.xml");
     }
@@ -38,7 +43,7 @@ public class SmokeTest extends AbstractCdiTest {
     @Deployment(name = "cdi", testable = false)
     public static WebArchive createCdiServletEnabledDeployment() {
         return ArchiveProvider.createWebArchive("cdi-test",
-                CdiView.class);
+                CdiView.class, ProxiedNavigationTargetView.class, UselessInterceptor.class);
     }
 
     @Before
@@ -57,6 +62,38 @@ public class SmokeTest extends AbstractCdiTest {
     @OperateOnDeployment("cdi")
     public void injectionHappensWithEnabledCdiServlet() {
         assertLabelEquals("hello CDI");
+    }
+
+    @Test
+    @OperateOnDeployment("cdi")
+    public void navigationCorrectlyHandlesProxiedViews() {
+        getDriver().get(getTestURL() + "proxied");
+        waitForDevServer();
+
+        String prevUuid = null;
+        AtomicReference<String> prevCounter = new AtomicReference<>("");
+        for (int i = 0; i < 5; i++) {
+            String uuid = waitUntil(d -> d.findElement(By.id("COMPONENT_ID")))
+                    .getText();
+
+            waitUntil(d -> !prevCounter.get()
+                    .equals(d.findElement(By.id("CLICK_COUNTER")).getText()));
+
+            if (prevUuid != null) {
+                Assert.assertEquals("UUID should not have been changed",
+                        prevUuid, uuid);
+            } else {
+                prevUuid = uuid;
+            }
+            String counter = findElement(By.id("CLICK_COUNTER")).getText();
+            Assert.assertEquals(
+                    "Parameter and counter should have the same value",
+                    "P:" + i + ", C:" + i, counter);
+
+            prevCounter.set(counter);
+
+            $("a").first().click();
+        }
     }
 
     private void assertLabelEquals(String expected) {

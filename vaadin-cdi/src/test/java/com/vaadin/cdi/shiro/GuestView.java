10d50b8d7f040363db5365651872a189078b8c49
Patrik Lindström <patrik@vaadin.com>
2026-05-25 16:32:52 +0300

    chore: Update copyright headers

--- vaadin-cdi/src/test/java/com/vaadin/cdi/shiro/GuestView.java
+++ vaadin-cdi/src/test/java/com/vaadin/cdi/shiro/GuestView.java
@@ -1,3 +1,13 @@
+/*
+ * Vaadin CDI Add-on
+ *
+ * Copyright (C) 2012-2026 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
 package com.vaadin.cdi.shiro;
 
 import com.vaadin.cdi.CDIView;
